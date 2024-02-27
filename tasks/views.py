8eafa210ed45fa73bc2fa9d18b6be2cbf41f30b3
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-02-27 14:32:11 -0300

    Add Django REST Framework to tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task, Group
+from .serializers import TaskSerializer, GroupSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
