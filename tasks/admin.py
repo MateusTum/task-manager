6b0e5cdc1de8ae6a3b40221ee26c1a89cae96477
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-02-27 14:08:04 -0300

    Add priority to tasks model

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,7 +4,7 @@ from .models import Task, Group
 # Register your models here.
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'completed', 'created_at', 'updated_at', 'group')
+    list_display = ('name', 'completed', 'priority', 'created_at', 'updated_at', 'group')
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Group)
--- tasks/models.py
+++ tasks/models.py
@@ -10,12 +10,22 @@ class Group(models.Model):
         return self.name
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('L', 'Low'),
+        ('N', 'Normal'),
+        ('H', 'High'),
+        ('VH', 'Very High'),
+    ]
+
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    priority = models.CharField(max_length=2, choices=PRIORITY_CHOICES, default='N')
 
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='tasks')
 
