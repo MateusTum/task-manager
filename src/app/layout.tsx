a170f2addc0a65a21f4b712f4073b953cbd2d24e
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-14 08:56:27 -0300

    Moved font to to fonts.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { roboto } from '@/app/ui/fonts';
 import "./ui/globals.css";
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
