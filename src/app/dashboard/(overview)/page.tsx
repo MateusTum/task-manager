ad9d0fa07f1d7bdbf0f8e8b8130752af5e6de2d5
MateusTum <83322634+MateusTum@users.noreply.github.com>
2024-03-14 09:33:42 -0300

    Create page.tsx

---
+++ src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,11 @@
+import { roboto } from '@/app/ui/fonts';
+
+export default async function Page() {
+    return (
+      <main>
+        <h1 className={`${roboto.className} mb-4 text-xl md:text-2xl`}>
+          Your tasks
+        </h1>
+      </main>
+    );
+  }
